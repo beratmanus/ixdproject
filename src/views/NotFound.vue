324cb21d6f1244f3aab27fb54e57e2e442c614b2
Chiara Flaminio <chiara.flaminio@student.pxl.be>
2022-05-22 16:26:24 +0200

    updated not found page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,5 +1,13 @@
 <template>
-  <h2>404</h2>
-  <h3>Page not found</h3>
+  <div class="content">
+    <h2 style="color: #af3838">404</h2>
+    <h3>Page not found...</h3>
+    <a href="" @click="this.$router.push('wijnen')">Terug naar wijnenlijst</a>
+  </div>
 </template>
 
+<style>
+.content{
+  margin-top: 75%;
+}
+</style>
