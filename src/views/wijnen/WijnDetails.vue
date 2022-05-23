75e3d99eb58fb11dddf6e98654b988344d6a2851
Chiara Flaminio <chiara.flaminio@student.pxl.be>
2022-05-23 23:58:17 +0200

    last commit for today, updated wijndetails again

--- src/views/wijnen/WijnDetails.vue
+++ src/views/wijnen/WijnDetails.vue
@@ -1,14 +1,17 @@
 <template>
-  <img :src="backfull" alt="back button" @click="this.$router.back()">
-  <img id="bg" :src="bg_img" alt="wijn background">
+  <img class="heading" id="back" :src="backfull" alt="back button" @click="this.$router.back()">
+  <img class="heading" id="bg" :src="bg_img" alt="wijn background">
   <div class="titles">
     <img id="country" :src="itflag" alt="IT flag">
-    <h3>Italië</h3>
-    <h2>{{ regio }}</h2>
+    <p>Italië</p>
+    <h3>{{ regio }}</h3>
     <h1>{{ title }}</h1>
   </div>
-  <div class="buttons">
-    <button class="">Toevoegen aan lijst</button>
+  <div class="content">
+    <hr>
+    <div class="buttons">
+      <button class="">Toevoegen aan lijst</button>
+    </div>
   </div>
 </template>
 
@@ -17,7 +20,7 @@ export default {
   props: ['id', 'title', 'details', 'img', 'bg_img', 'regio']
   ,
   data() {
-    return{
+    return {
       itflag: require("/public/images/IT.png"),
       backfull: require("/public/images/back_fullblack.png")
     }
@@ -30,21 +33,63 @@ button {
   border-radius: 12px;
   padding: 18px;
 }
-#bg{
+
+hr {
+  border: 0.5px rgba(222, 215, 215, 0.4) solid;
+  width: 90%;
+}
+
+.heading {
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+
+#back {
+  z-index: 3;
+  margin-top: 50px;
+  margin-left: 15px;
+}
+
+#bg {
   width: 100%;
   height: auto;
+  z-index: 1;
 }
-#country{
-  width: 5%;
-  height: 5%;
+
+#country {
+  width: 10%;
+  height: 10%;
   display: inline;
 }
-.titles{
+
+.titles {
   display: inline;
   float: start;
   text-align: left;
+  position: fixed;
+  top: 275px;
+  left: 15px;
 }
-h3{
-display: inline-block;
+
+p {
+  display: inline-block;
+}
+
+h3 {
+  margin: 0;
+  padding: 0;
+  color: #8c8c8c;
+  font-weight: normal;
+}
+
+h1 {
+  margin: 0;
+  padding: 0;
+}
+
+.content {
+  top: 500px;
+  margin-top: 425px;
 }
 </style>
